f4d9e53bfee4765915646dd7755a41d10f272c03
Amine Lemaizi <40530011+aminelemaizi@users.noreply.github.com>
2024-09-12 23:53:21 +0100

    base session variables bricks

---
+++ Bricks/get_session_variable.py
@@ -0,0 +1,14 @@
+from coded_flows.types import Any
+
+
+coded_flows_metadata = {
+    "display_name": "Get Session Variable",
+    "description": "Get a session variable value",
+    "type": "svariable",
+    "icon": "code",
+}
+
+
+def get_session_variable() -> Any:
+    value = ""
+    return value
